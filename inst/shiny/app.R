e3789d1ee3a4528ff183e87a12e145f481d66e53
Romain Francois <romain@thinkr.fr>
2017-07-05 20:59:54 +0200

    debut appli shiny

--- inst/shiny/app.R
+++ inst/shiny/app.R
@@ -2,36 +2,45 @@ library(shiny)
 library(leaflet)
 library(bac2017)
 library(prenoms)
+library(sf)
 
-ui <- fluidPage(
+ui <- fluidPage(theme = "bac.css",
 
-   # Sidebar with a slider input for number of bins
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
+  div( class = "fullpage",
+    leafletOutput("carte", width = "100%", height = "100%")
+  )
 
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
 )
 
 server <- function(input, output) {
 
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2]
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
+  admis <- reactive({
+    bac2017 %>%
+      group_by( numero_departement ) %>%
+      summarise(
+        nom_departement = first(nom_departement),
+        p_admis = sum(Resultat != "PASSE SECOND GROUPE" ) / n()
+      )
+  })
 
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+  output$carte <- renderLeaflet({
+    data <- left_join( departements, admis(), by = c("code_insee" = "numero_departement") ) %>%
+      mutate(
+        col = gray( 1 - ( p_admis - min(p_admis) ) / (max(p_admis) - min(p_admis))),
+        label = sprintf( "%s (%4.2f)", nom_departement, round(p_admis * 100, 2) )
+      )
+
+    leaflet(data) %>%
+      addTiles( urlTemplate = 'http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png' ) %>%
+      setView(lng = 5, lat= 47, zoom=7) %>%
+      addPolygons( color = "black", weight = .5, fillColor = ~col, fill = TRUE, fillOpacity = .7, label = ~label,
+        labelOptions = labelOptions(
+          style = list("font-weight" = "normal", padding = "3px 8px"),
+          textsize = "15px",
+          direction = "auto"
+        )
+      )
+  })
 }
 
 # Run the application
